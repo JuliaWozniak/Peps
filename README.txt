b4e2471846ef36934fef5e219566d8c593c594f9
Julia Wozniak <jwozniak@Julias-MacBook-Pro.local>
2021-06-21 22:26:22 +0300

    hist

---
+++ README.txt
@@ -0,0 +1,12 @@
+1. run the program
+python3 main.py
+
+3. copy the address that appears in terminal and paste into web browser
+
+4. choose region
+
+5. wait a bit
+
+
+The histogram shows the total amount of declared income of 100 judges of the selected region.
+(Many have not specified their income, so the most frequent amount is 0).
--- main.py
+++ main.py
@@ -3,26 +3,120 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import requests
+import json
+import plotly.express as px
 
-df = pd.read_csv('twenty.csv')
+
+region = "м.+Київ"
+
+
+
+def get_income(p):
+    if 'step_11' in p['unified_source']:
+        income = p['unified_source']['step_11']
+    elif 'step_2' in p['unified_source']:
+        income = p['unified_source']['step_2']
+    else:
+        return (0)
+    kk = income.keys()
+    total_income = 0
+    for k in kk:
+        try:
+            item = income[k]
+            if 'sizeIncome' in item:
+                total_income += float(item['sizeIncome'])
+        except KeyError:
+            print('income') 
+    return(total_income)
+
+def get_estate(p):
+    if 'step_3' in p['unified_source']:
+        income = p['unified_source']['step_3']
+    else:
+        return ((0,0))
+    kk = income.keys()
+    total_area = 0
+    total_cost = 0
+    for k in kk:
+        try:
+            item = income[k]
+            item_dict = {'area':0}
+            if 'totalArea' in item:
+                total_area += float(item['totalArea'])
+            if 'costAssessment' in item:
+                total_cost += float(item['costAssessment'])
+        except KeyError as e:
+            print(e)
+    return((total_area, total_cost))
+
+def get_cars(p):
+    if 'step_6' in p['unified_source']:
+        income = p['unified_source']['step_6']
+    else:
+        return (0)
+    kk = income.keys()
+    income_list = []
+    total_cost = 0
+    for k in kk:
+        item = income[k]
+        if 'costDate' in item:
+            total_cost += int(item['costDate'])
+    return(total_cost)
+
+
+def download_data(reg):
+	region = str(reg)
+	print('here')
+	data = []
+	r = requests.get("https://declarations.com.ua/search?q=суддя&deepsearch=on&declaration_year%5B%5D=2019&region_value%5B%5D" + region + "&format=opendata").json()
+	data += r["results"]["object_list"]
+	for page in range(2, 5):
+		subr = requests.get("https://declarations.com.ua/search?q=суддя&deepsearch=on&declaration_year%5B%5D=2019&region_value%5B%5D=" + region + "&format=opendata&page=%s" % page).json()
+		data += subr["results"]["object_list"]
+	new_df = []
+	for p in data:
+		new = {'guid':0}
+		new['guid'] = p['guid']
+		try:
+			new['total_income'] = get_income(p)
+		except KeyError:
+			print('income' + p['guid'] + a)
+		try:
+			estate = get_estate(p)
+			new['total_area'] = estate[0]
+			new['estate_cost'] = estate[1]
+		except KeyError :
+			print('estate'+ p['guid'] + a)
+		try:
+			new['cost_cars'] = get_cars(p)
+		except KeyError:
+			print(p['guid'])
+		new_df += [new]
+	df = pd.DataFrame(new_df)
+	
+	return (df)
+
+
+df = download_data(region)
 
 app = dash.Dash("__main_PEP__")
-risks = df.risk.unique()
+regions = ["м.+Київ", "Львівська+область","Одеська+область"]
 server = app.server
 app.layout = html.Div([
-    html.Div([dcc.Dropdown(id='group-select', options=[{'label': i, 'value': i} for  i in risks],
-                           value='TOR', style={'width': '140px'})]),
-    dcc.Graph('shot-dist-graph', config={'displayModeBar': False})])
+    html.Div([
+	dcc.Dropdown(id='group-select', options=[{'label': reg, 'value': reg} for  reg in regions],
+                            value='м.+Київ', style={'width': '140px'})]),
+     dcc.Graph('shot-dist-graph', config={'displayModeBar': False})])
 
 
 @app.callback(
-    Output('shot-dist-graph', 'figure'),
-    [Input('group-select', 'value')]
-)
-def update_graph(grpname):
-    import plotly.express as px
-    return px.scatter(df[df['risk']==grpname], x='score', y='total_property_assets', size='total_property_assets_divide_income', color='1')
+	dash.dependencies.Output('shot-dist-graph', 'figure'),
+        [dash.dependencies.Input('group-select', 'value')])
 
+def update_graph(reg):
+	df = download_data(reg)
+	return px.histogram(df, "total_income")
 if __name__ == '__main__':
+     app.run_server(debug=True, port=49091)
 
-    app.run_server(debug=False)
